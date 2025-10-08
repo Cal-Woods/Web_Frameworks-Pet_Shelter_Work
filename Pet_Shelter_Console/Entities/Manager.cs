7d2568f2b8dd42d4959e6064ea29f2e2795bdcd2
Cal Woods <D00270955@student.dkit.ie>
2025-10-08 20:48:14 +0100

    Feat: Added fields to be used as object instance attributes and I added properties to get & set each field.
    Fields are: int managerId, string name, double pay to represent the manager id, name of manager & amount paid.
    
    All fields are private with public accessors.

---
+++ Pet_Shelter_Console/Entities/Manager.cs
@@ -0,0 +1,25 @@
+﻿namespace Entities
+{
+    public class Manager
+    {
+        private int managerId;
+        private string name;
+        private double pay;
+
+        public int ManagerId
+        {
+            get { return managerId; }
+            set { managerId = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Pay
+        {
+            get { return pay; }
+            set { pay = value; }
+        }
+    }
+}
