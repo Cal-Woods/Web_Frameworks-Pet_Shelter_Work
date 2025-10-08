58b595e0afa3a8078f0ae23389e0c6b673cc2ea6
Cal Woods <D00270955@student.dkit.ie>
2025-10-08 20:18:49 +0100

    Feat: Added class declaration to Program and implemented a print message in main() method.
    Added public class Program and public static void Main() with a print statement stating the purpose of the console application.
    
    May implement GUI later.

--- Pet_Shelter_Console/Program.cs
+++ Pet_Shelter_Console/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
-internal class Program
+public class Program
 {
-
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("This is an application for a pet shelter management system! This system will allow managers to enter new animal records into system\n  for vets to be able to view these records.\nFosterers will be able to view animals that need a home and filter search results. ");
+    }
 }
