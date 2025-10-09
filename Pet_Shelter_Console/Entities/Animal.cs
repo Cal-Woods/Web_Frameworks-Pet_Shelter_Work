e229aa4ecc5dc5697711c89d5e90958f2cf914e8
Cal Woods <D00270955@student.dkit.ie>
2025-10-09 20:57:37 +0100

    Feat: Added properties for all attributes.
    Added properties to get & set each value without directly touching the private fields.

--- Pet_Shelter_Console/Entities/Animal.cs
+++ Pet_Shelter_Console/Entities/Animal.cs
@@ -7,5 +7,31 @@
         private double height;
         private double width;
         private char sex;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public double Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public char Sex
+        {
+            get { return sex; }
+            set { sex = value; }
+        }
     }
 }
