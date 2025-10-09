1068f46e7698410a656bbcd8579228bc85d78029
Cal Woods <D00270955@student.dkit.ie>
2025-10-09 20:04:31 +0100

    Feat: Added two constructors for parameterised & no parameters.
    Two new class constructors added to Manager class. One constructor takes no parameters and initialises the three attributes to obviously fake values whereas the second constructor takes three parameters & sets each attribute to it's matching parameter.
    
    No parameter constructor for testing only!

--- Pet_Shelter_Console/Entities/Manager.cs
+++ Pet_Shelter_Console/Entities/Manager.cs
@@ -21,5 +21,28 @@
             get { return pay; }
             set { pay = value; }
         }
+        /// <summary>
+        /// No parameter constructor that sets all attributes to dummy values!
+        /// </summary>
+        public Manager()
+        {
+            ManagerId = -1;
+            Name = "Dummy Manager object for testing, change all values before production usage!";
+            Pay = -1.00;
+
+        }
+
+        /// <summary>
+        /// A parameterised constructor for instantiating a Manager object with given values!
+        /// </summary>
+        /// <param name="managerId">Given manager id</param>
+        /// <param name="name">Given name</param>
+        /// <param name="pay">Given pay of manager</param>
+        public Manager(int managerId, string name, double pay)
+        {
+            ManagerId = managerId;
+            Name = name;
+            Pay = pay;
+        }
     }
 }
