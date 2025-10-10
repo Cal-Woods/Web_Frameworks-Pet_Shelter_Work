365b8fa0a3fc401d4f1efb77bb4806bb3e1e0279
Cal Woods <D00270955@student.dkit.ie>
2025-10-10 11:55:50 +0100

    Feat: Added variable fields and properties for accessing them.
    Added private fields general to all employees with properties for public access.
    
    Fields are: string employeeId, string name, double salary, string employeeType, string location.

--- Pet_Shelter_Console/Entities/Employee.cs
+++ Pet_Shelter_Console/Entities/Employee.cs
@@ -2,6 +2,34 @@
 {
     public abstract class Employee
     {
-
+        private string employeeId;
+        private string name;
+        private double salary;
+        private string employeeType;
+        private string location;
+        public string EmployeeId {
+            get { return employeeId; } 
+            set { employeeId = value; }
+        }
+        public string Name 
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Salary 
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        public string EmployeeType 
+        {
+            get { return employeeType; }
+            set { employeeType = value; }
+        }
+        public string Location 
+        {
+            get { return location; }
+            set { location = value; }
+        }
     }
 }
