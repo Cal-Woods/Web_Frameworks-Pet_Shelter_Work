07e2e0b286eb3dcf5ee05eb7e39fd841c57fd794
Cal Woods <D00270955@student.dkit.ie>
2025-10-10 16:19:49 +0100

    Refactor: Removed properties and fields as they are now contained in the Employee derived class
    Added Employee abstract class derive statement to class declaration.

--- Pet_Shelter_Console/Entities/Manager.cs
+++ Pet_Shelter_Console/Entities/Manager.cs
@@ -1,48 +1,25 @@
 ﻿namespace Entities
 {
-    public class Manager
+    public class Manager : Employee
     {
-        private int managerId;
-        private string name;
-        private double pay;
 
-        public int ManagerId
-        {
-            get { return managerId; }
-            set { managerId = value; }
-        }
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-        public double Pay
-        {
-            get { return pay; }
-            set { pay = value; }
-        }
         /// <summary>
-        /// No parameter constructor that sets all attributes to dummy values!
+        /// No parameter constructor to call base constructor Employee, that sets all attributes to dummy values!
         /// </summary>
-        public Manager()
+        public Manager() : base()
         {
-            ManagerId = -1;
-            Name = "Dummy Manager object for testing, change all values before production usage!";
-            Pay = -1.00;
-
         }
 
         /// <summary>
         /// A parameterised constructor for instantiating a Manager object with given values!
         /// </summary>
-        /// <param name="managerId">Given manager id</param>
-        /// <param name="name">Given name</param>
-        /// <param name="pay">Given pay of manager</param>
-        public Manager(int managerId, string name, double pay)
+        /// <param name="employeeId">Given employee id of manager</param>
+        /// <param name="name">Given name of manager</param>
+        /// <param name="salary">Given pay of manager</param>
+        /// <param name="employeeType">Given type of manager</param>
+        /// <param name="location">Given location of manager</param>
+        public Manager(string employeeId, string name, double salary, string employeeType, string location) : base(employeeId, name, salary, employeeType, location)
         {
-            ManagerId = managerId;
-            Name = name;
-            Pay = pay;
         }
     }
 }
