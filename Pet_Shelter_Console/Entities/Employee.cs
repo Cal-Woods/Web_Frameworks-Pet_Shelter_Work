43be25a869e33d54aa50c67430f488aaa8f881a7
Cal Woods <D00270955@student.dkit.ie>
2025-10-10 12:48:09 +0100

    Feat: Added two constructors: no parameter & parameterised.
    Added two new constructors to Employee with protected access modifiers.
    
    To avoid duplicate creation of properties in classes that derive Employee.

--- Pet_Shelter_Console/Entities/Employee.cs
+++ Pet_Shelter_Console/Entities/Employee.cs
@@ -7,29 +7,50 @@
         private double salary;
         private string employeeType;
         private string location;
-        public string EmployeeId {
+        protected string EmployeeId {
             get { return employeeId; } 
             set { employeeId = value; }
         }
-        public string Name 
+        protected string Name 
         {
             get { return name; }
             set { name = value; }
         }
-        public double Salary 
+        protected double Salary 
         {
             get { return salary; }
             set { salary = value; }
         }
-        public string EmployeeType 
+        protected string EmployeeType 
         {
             get { return employeeType; }
             set { employeeType = value; }
         }
-        public string Location 
+        protected string Location 
         {
             get { return location; }
             set { location = value; }
+        }
+
+        /// <summary>
+        /// A no parameter constructor that instantiates test properties for an employee.
+        /// </summary>
+        /// <remarks>Not to be used in production!</remarks>
+        protected Employee() 
+        {
+            EmployeeId = "-1";
+            Name = "Dummy Manager object for testing, change all values before production usage!";
+            Salary = -1.00;
+            EmployeeType = "Test";
+            Location = "Dummy Lane, Co. Louth, Ireland";
+        }
+        protected Employee(string employeeId, string name, double salary, string employeeType, string location)
+        {
+            EmployeeId = employeeId;
+            Name = name;
+            Salary = salary;
+            EmployeeType = employeeType;
+            Location = location;
         }
     }
 }
