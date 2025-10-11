89ca8e04f8800b52ced0b0539e168f28f1cbe76d
Cal Woods <D00270955@student.dkit.ie>
2025-10-11 17:40:12 +0100

    Refactor: Moved MySqlConnection declaration to top of class & moved MySqlConnection constuctor parameter to top of constructor.
    Try block now sets connection attribute to given MySqlConnection.

--- Pet_Shelter_Console/DAOs/ConnectionFacilitator.cs
+++ Pet_Shelter_Console/DAOs/ConnectionFacilitator.cs
@@ -5,20 +5,21 @@ namespace DAOs
 {
     public class ConnectionFacilitator
     {
+        MySqlConnection connection;
         public ConnectionFacilitator(MySqlConnection conn)
         {
-            MySqlConnection connection;
+            if (conn == null)
+            {
+                throw new ArgumentNullException("MySql connection object was null, must be initialised!");
+            }
+            if (conn.ConnectionString == null)
+            {
+                throw new ArgumentNullException("MySql connection object connection string was not set, set connection string in format: 'server=url;uid=username;pwd=password;database=dbName'");
+            }
+
+            //Try to assign connection
             try
             {
-                if (conn == null)
-                {
-                    throw new ArgumentNullException("MySql connection object was null, must be initialised!");
-                }
-                if (conn.ConnectionString == null)
-                {
-                    throw new ArgumentNullException("MySql connection object connection string was not set, set connection string in format: 'server=url;uid=username;pwd=password;database=dbName'");
-                }
-
                 connection = conn;
                 connection.Open();
             }
@@ -26,6 +27,12 @@ namespace DAOs
             {
                 Console.WriteLine("Connection to db could not be established, check given MySqlConnection object or sql database!\n"+e.Message);
             }
+        }
+
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+            private set { connection = value; }
         }
     }
 }
