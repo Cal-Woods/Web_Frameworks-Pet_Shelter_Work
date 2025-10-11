6cf1d0d3d8ab0a3ad8f7e404863222a15af41e8f
Cal Woods <D00270955@student.dkit.ie>
2025-10-11 17:19:04 +0100

    Feat: Added class ConnectionFacilitator.
    New class to facilitate mysql connection to a specific db by using a MySqlConnection object assigned to a MySqlConnection instance attribute.
    
    Throws ArgumentNullException if given MySqlConnection object is null or the connection string is null.

---
+++ Pet_Shelter_Console/DAOs/ConnectionFacilitator.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace DAOs
+{
+    public class ConnectionFacilitator
+    {
+        public ConnectionFacilitator(MySqlConnection conn)
+        {
+            MySqlConnection connection;
+            try
+            {
+                if (conn == null)
+                {
+                    throw new ArgumentNullException("MySql connection object was null, must be initialised!");
+                }
+                if (conn.ConnectionString == null)
+                {
+                    throw new ArgumentNullException("MySql connection object connection string was not set, set connection string in format: 'server=url;uid=username;pwd=password;database=dbName'");
+                }
+
+                connection = conn;
+                connection.Open();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine("Connection to db could not be established, check given MySqlConnection object or sql database!\n"+e.Message);
+            }
+        }
+    }
+}
