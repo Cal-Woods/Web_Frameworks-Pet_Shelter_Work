ab3a86e16566df5ec3f1eeab688dba75d2b68e3b
Cal Woods <D00270955@student.dkit.ie>
2025-10-12 14:31:26 +0100

    Refactor/Feat(SQL): Changed DECIMAL parameters in file to have even split.
    Changed DECIMAL declarations to be 4,2/8,2.
    
    Added INSERT command for employees table.

--- Pet_Shelter_Console/Resources/CREATE-INSERT_SQL.sql
+++ Pet_Shelter_Console/Resources/CREATE-INSERT_SQL.sql
@@ -4,17 +4,21 @@ USE `web-frameworks_CA1-db`;
 CREATE TABLE IF NOT EXISTS `employees`(
   EmployeeId INT(20) PRIMARY KEY,
   Name VARCHAR(255),
-  Salary DECIMAL(5,2),
-  salaryType VARCHAR(255),
+  Salary DECIMAL(8,2),
+  employeeType VARCHAR(255),
   location VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS `animals`(
   animalId INT(20) PRIMARY KEY,
   name VARCHAR(255),
-  age INT(2),
-  height DECIMAL(4, 2),
-  width DECIMAL(4, 2),
+  age INT(20),
+  height DECIMAL(4,2),
+  width DECIMAL(4,2),
   sex CHAR(1),
   species VARCHAR(255)
 );
+
+/*INSERT commands
+  employees*/
+INSERT INTO `employees` (`EmployeeId`, `Name`, `Salary`, `salaryType`, `location`) VALUES ('20', 'Cal Derek John Farrell Woods', '5000.50', 'Manager', 'Here');
