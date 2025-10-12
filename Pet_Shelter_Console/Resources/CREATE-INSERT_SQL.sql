d9041d19ae20ee9bdaecc40f755aca7aaaf85f87
Cal Woods <D00270955@student.dkit.ie>
2025-10-12 12:34:03 +0100

    Feat(SQL): Added sql code to create table animals.
    The animals table contains columns for: name, age, height, width, sex & species of animal.

--- Pet_Shelter_Console/Resources/CREATE-INSERT_SQL.sql
+++ Pet_Shelter_Console/Resources/CREATE-INSERT_SQL.sql
@@ -1,10 +1,20 @@
 CREATE DATABASE IF NOT EXISTS `web-frameworks_CA1-db`;
-USE `web-frameworks_CA1-db`
+USE `web-frameworks_CA1-db`;
 
 CREATE TABLE IF NOT EXISTS `employees`(
-  EmployeeId INT(2) PRIMARY KEY,
+  EmployeeId INT(20) PRIMARY KEY,
   Name VARCHAR(255),
   Salary DECIMAL(5,2),
   salaryType VARCHAR(255),
-  location VARCHAR(25)
+  location VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS `animals`(
+  animalId INT(20) PRIMARY KEY,
+  name VARCHAR(255),
+  age INT(2),
+  height DECIMAL(4, 2),
+  width DECIMAL(4, 2),
+  sex CHAR(1),
+  species VARCHAR(255)
 );
