461edb2fe9011cc9ccc38291a0ee036e29ac08ea
Cal Woods <D00270955@student.dkit.ie>
2025-10-12 14:41:32 +0100

    Feat: Added welcome message to top of Main() method.
    Simple print statement that states the purpose of the program.

--- Pet_Shelter_Console/Program.cs
+++ Pet_Shelter_Console/Program.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using DAOs;
+using Entities;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("This is an application for a pet shelter management system! This system will allow managers to enter new animal records into system\n  for vets to be able to view these records.\nFosterers will be able to view animals that need a home and filter search results. ");
+        Console.WriteLine("This is an application for a pet shelter management system! This system will allow managers to enter new animal records into system\nFor vets to be able to view these records.\nFosterers will be able to view animals that need a home and filter search results.");
     }
 }
