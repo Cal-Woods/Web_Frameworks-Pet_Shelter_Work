f196647c203761b4f01cf4ed596de25f5b54c046
Cal Woods <D00270955@student.dkit.ie>
2025-10-12 21:08:51 +0100

    Feat: Added new class ManagersDAO with a constructor that takes a ConnectionFacilitator object.
    Will hold all needed managers db table actions.
    
    There is no property for instance ConnectionFacilitator as I do not want it accessed directly.

---
+++ Pet_Shelter_Console/DAOs/ManagersDAO.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Entities;
+using MySql.Data.MySqlClient;
+using System.Collections;
+
+namespace DAOs
+{
+    public class ManagersDAO
+    {
+        private ConnectionFacilitator connectionFacilitator;
+        public ManagersDAO(ConnectionFacilitator conn) 
+        {
+            if (conn == null) 
+            {
+                throw new ArgumentNullException("Given ConnectionFacilitator object was null! Please provide a valid ConnectionFacilitator!");
+            }
+
+            this.connectionFacilitator = conn;
+        }
+    }
+}
