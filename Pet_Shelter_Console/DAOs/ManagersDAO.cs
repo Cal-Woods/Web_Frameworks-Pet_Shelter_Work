0677d7e2d5b8b7e7240774c053b1d11b179bd03c
Cal Woods <D00270955@student.dkit.ie>
2025-10-12 21:44:12 +0100

    Feat: Added method GetAllManagers() to get all manager records from database Pet_Shelter and store them in a List object of type Manager.
    Method GetAllManagers() instantiates a list of type Manager object and uses two try-catch blocks to create MySqlCommand & MySqlDataReader instances to prepare command & execute command. The execute command returns a set of retrieved data from a mysql database. This method returns a List of Managers gotten from db.
    
    Ran into trouble with MySqlReader object as it would not allow multiple retrieve operations. Solution was to implement the Close() method after retrievig all data from database.

--- Pet_Shelter_Console/DAOs/ManagersDAO.cs
+++ Pet_Shelter_Console/DAOs/ManagersDAO.cs
@@ -18,5 +18,39 @@ namespace DAOs
 
             this.connectionFacilitator = conn;
         }
+
+        public List<Manager> GetAllManagers()
+        {
+            List<Manager> managerList = new List<Manager>();
+
+            try
+            {
+                MySqlCommand comm = new MySqlCommand("SELECT * FROM employees", connectionFacilitator.Connection);
+
+                try
+                {
+
+                    MySqlDataReader data = comm.ExecuteReader();
+
+                    while (data.Read())
+                    {
+                        managerList.Add(new Manager(data[0].ToString(), data[1].ToString(), Double.Parse(data[2].ToString()), data[3].ToString(), data[4].ToString()));
+                    }
+                    data.Close();
+
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine("2nd catch\n" + e.Message);
+                }
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("1st catch\n" + ex.Message);
+            }
+
+            return managerList;
+        }
     }
 }
