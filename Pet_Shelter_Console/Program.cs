5f11be50cc3ae89f89c922a104e4ae639dba2b4f
Cal Woods <D00270955@student.dkit.ie>
2025-10-14 21:24:44 +0100

    Feat: Added method StoreManagers(ManagersDAO managersDb) to take a ManagersDAO Object and use it for accessing database to utilise GetAllManagers() method, then return a List<Manager> with all manager records.
    Method checks if Program class List<Manager> count = 0, then use GetAllManagers() method to store an updated List. Includes an option to update List either way by requesting user input 'y' to update, 'n' to finish method.
    
    Method is private & static as it is accessed by the program Main() method only.

--- Pet_Shelter_Console/Program.cs
+++ Pet_Shelter_Console/Program.cs
@@ -1,11 +1,13 @@
 ﻿using DAOs;
 using Entities;
 using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Security;
 using System;
 using System.Data;
 
 public class Program
 {
+    private static List<Manager> list;
     public static void Main(string[] args)
     {
         Console.WriteLine("This is an application for a pet shelter management system! This system will allow managers to enter new animal records into system\nFor vets to be able to view these records.\nFosterers will be able to view animals that need a home and filter search results.");
@@ -14,11 +16,17 @@ public class Program
         string? menuChoice = "";
 
         const string SENTINEL = "exit";
-        while (isRunning) {
+
+        ManagersDAO dao = new ManagersDAO(new ConnectionFacilitator());
+
+        list = StoreManagers(dao);
+
+        while (isRunning)
+        {
             Console.WriteLine("\nPlease choose from the following options by typing a number:\n1) View all Employee records\n2) View All Animal records\n3) Add Employee to database\n4)Add Animal to database\nType 'exit' to exit program");
             menuChoice = Console.ReadLine();
 
-            switch (menuChoice.ToLower()) 
+            switch (menuChoice.ToLower())
             {
                 case "1":
                     //TODO:Call method for corresponding action
@@ -45,5 +53,33 @@ public class Program
         }
 
         return;
+    }
+
+    private static List<Manager> StoreManagers(ManagersDAO managersDb)
+    {
+        if (managersDb == null)
+        {
+            throw new ArgumentNullException("Given ManagersDAO object was null, please check passed parameter.");
+        }
+
+        if (list.Count == 0)
+        {
+            Console.WriteLine("The list of Managers is empty, fetching all manager records from database...");
+            list = managersDb.GetAllManagers();
+        }
+        else
+        {
+            Console.Write("list is already populated.\nWould you like to fetch from database anyway?(y/n) ");
+            if (Console.Read().Equals("y"))
+            {
+                list = managersDb.GetAllManagers();
+            }
+            else
+            {
+                Console.WriteLine("Done.");
+            }
+        }
+
+        return list;
     }
 }
