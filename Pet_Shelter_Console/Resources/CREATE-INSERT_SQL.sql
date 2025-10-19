999d0d3ad9979c19ba4f163a73222eae2274b76b
Cal Woods <D00270955@student.dkit.ie>
2025-10-19 18:33:07 +0100

    Feat(SQL): Added animals INSERT statements at end of file.
    One of them is a test which I labelled 'BadSpecies_Test' for testing incoming animals species entry against class Animal species enum.
    
    Also for table population.

--- Pet_Shelter_Console/Resources/CREATE-INSERT_SQL.sql
+++ Pet_Shelter_Console/Resources/CREATE-INSERT_SQL.sql
@@ -25,3 +25,5 @@ INSERT INTO `employees` (`employeeId`, `name`, `salary`, `employeeType`, `locati
 
 /*INSERT commands animals*/
 INSERT INTO `animals` (`animalId`,`name`,`age`,`height`,`width`,`sex`,`species`) VALUES ("MaxD0g", "Max", 4, 3.45, 2.00, 'M', "Dog");``,
+INSERT INTO `animals` (`animalId`, `name`, `age`, `height`, `width`, `sex`, `species`) VALUES ('AParr0t', 'Crackers', '2', '4.1', '3.8', 'F', 'Parrot');
+INSERT INTO `animals` (`animalId`, `name`, `age`, `height`, `width`, `sex`, `species`) VALUES ('BadSpecies_Test', 'Bonza', '8', '23', '35', 'U', 'Parogee');
