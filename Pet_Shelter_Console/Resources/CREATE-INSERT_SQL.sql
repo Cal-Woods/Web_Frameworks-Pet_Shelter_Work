c6443fc87773b842c02a88424c833ba0df3d414c
Cal Woods <D00270955@student.dkit.ie>
2025-10-19 19:58:50 +0100

    Feat(SQL):  Added new table with one column and added new INSERT statements.
    New table is called species with column name to represent species name.
    
    Added new INSERT statements.

--- Pet_Shelter_Console/Resources/CREATE-INSERT_SQL.sql
+++ Pet_Shelter_Console/Resources/CREATE-INSERT_SQL.sql
@@ -9,6 +9,11 @@ CREATE TABLE IF NOT EXISTS `employees`(
   location VARCHAR(255)
 );
 
+CREATE TABLE IF NOT EXISTS `species`
+(
+	name VARCHAR(255) PRIMARY KEY
+);
+
 CREATE TABLE IF NOT EXISTS `animals`(
   animalId VARCHAR(255) PRIMARY KEY,
   name VARCHAR(255),
@@ -16,14 +21,29 @@ CREATE TABLE IF NOT EXISTS `animals`(
   height DECIMAL(4,2),
   width DECIMAL(4,2),
   sex CHAR(1),
-  species VARCHAR(255)
+  species VARCHAR(255),
+  CONSTRAINT fk_species
+  FOREIGN KEY(species) REFERENCES species(name)
 );
+
 
 /*INSERT commands
   employees*/
 INSERT INTO `employees` (`employeeId`, `name`, `salary`, `employeeType`, `location`) VALUES ('20', 'Cal Derek John Farrell Woods', '5000.50', 'Manager', 'Here');
 
+/*INSERT commands species*/
+INSERT INTO species (name) VALUES ('Dog');
+INSERT INTO species (name) VALUES ('Cat');
+INSERT INTO species (name) VALUES ('Parrot');
+INSERT INTO species (name) VALUES ('Horse');
+INSERT INTO species (name) VALUES ('Fish');
+INSERT INTO species (name) VALUES ('Pig');
+INSERT INTO species (name) VALUES ('Pony');
+INSERT INTO species (name) VALUES ('Parogee');
+INSERT INTO species (name) VALUES ('Donkey');
+INSERT INTO species (name) VALUES ('None');
+
 /*INSERT commands animals*/
-INSERT INTO `animals` (`animalId`,`name`,`age`,`height`,`width`,`sex`,`species`) VALUES ("MaxD0g", "Max", 4, 3.45, 2.00, 'M', "Dog");``,
+INSERT INTO `animals` (`animalId`,`name`,`age`,`height`,`width`,`sex`,`species`) VALUES ("MaxD0g", "Max", 4, 3.45, 2.00, 'M', "Dog");
 INSERT INTO `animals` (`animalId`, `name`, `age`, `height`, `width`, `sex`, `species`) VALUES ('AParr0t', 'Crackers', '2', '4.1', '3.8', 'F', 'Parrot');
 INSERT INTO `animals` (`animalId`, `name`, `age`, `height`, `width`, `sex`, `species`) VALUES ('BadSpecies_Test', 'Bonza', '8', '23', '35', 'U', 'Parogee');
