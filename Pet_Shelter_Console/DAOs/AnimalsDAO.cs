92c11ba1241c799c7a1d8d5e1a8c3ed3c0784d9a
Cal Woods <D00270955@student.dkit.ie>
2025-10-19 20:35:50 +0100

    Feat: Added method to get all animals records from database.
    Added new method GetAllAnimals() to retrieve all animals from database as long as connection is open. Method creates a MySqlCommand with a sql query and a MySqlDataReader object to retrieve the results. This is all done within two try-catch blocks for catching possible MySqlException exceptions.
    
    Also added necessary using statements at top of file.

--- Pet_Shelter_Console/DAOs/AnimalsDAO.cs
+++ Pet_Shelter_Console/DAOs/AnimalsDAO.cs
@@ -1,4 +1,8 @@
-﻿namespace DAOs
+﻿using Entities;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DAOs
 {
     public class AnimalsDAO : IDAO
     {
@@ -8,6 +12,45 @@
             ArgumentNullException.ThrowIfNull(connectionFacilitator, "connectionFacilitator");
 
             connection = connectionFacilitator;
+        }
+
+        public List<Animal> GetAllAnimals()
+        {
+            if (connection.Connection.State == ConnectionState.Closed)
+            {
+                return new List<Animal>();
+            }
+
+            List<Animal> animalList = new List<Animal>();
+
+            try
+            {
+                MySqlCommand comm = new MySqlCommand("SELECT * FROM animals", connection.Connection);
+                try
+                {
+
+                    MySqlDataReader data = comm.ExecuteReader();
+
+                    while (data.Read())
+                    {
+                        animalList.Add(new Animal(data[0].ToString(), data[1].ToString(), Int32.Parse(data[2].ToString()), Double.Parse(data[3].ToString()), Double.Parse(data[4].ToString()), data[5].ToString()[0], data[6].ToString()));
+                    }
+
+                    data.Close();
+
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine("2nd catch\n" + e.Message);
+                }
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("1st catch\n" + ex.Message);
+            }
+
+            return animalList;
         }
     }
 }
