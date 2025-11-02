4fe3b8033848aa6e6b1dbb285b788264fa9467e2
Cal Woods <D00270955@student.dkit.ie>
2025-11-02 22:20:56 +0000

    Test: Added tests to EntityTests for testing Manager & Vet classes that derive from Employee.
    Both classes contain all fields in Employee.
    
    Tests successful.

--- Pet_Shelter_Console.UnitTests/EntityTests.cs
+++ Pet_Shelter_Console.UnitTests/EntityTests.cs
@@ -1,4 +1,5 @@
 using Entities;
+using Pet_Shelter_Console.Entities;
 
 namespace Pet_Shelter_Console.UnitTests
 
@@ -18,7 +19,44 @@ namespace Pet_Shelter_Console.UnitTests
             Assert.AreEqual(manager.EmployeeId, "-1");
             Assert.AreEqual(manager.Name, "Dummy Manager object for testing, change all values before production usage!");
             Assert.AreEqual(manager.Salary, -1.00);
+            Assert.AreEqual(manager.EmployeeType, "Test");
             Assert.AreEqual(manager.Location, "Dummy Lane, Co. Louth, Ireland");
+        }
+
+        [Test]
+        public void InstantiateManagerWithValidAllArgsConstructorSuccessful()
+        {
+            Manager manager = new Manager("first_managerAllArgs_test", "Dalton Reynolds", 45000.55, "Manager", "Drogheda, Co Louth, Ireland");
+
+            Assert.AreEqual(manager.EmployeeId, "first_test");
+            Assert.AreEqual(manager.Name, "Dalton Reynolds");
+            Assert.AreEqual(manager.Salary, 45000.55);
+            Assert.AreEqual(manager.EmployeeType, "Manager");
+            Assert.AreEqual(manager.Location, "Drogheda, Co Louth, Ireland");
+        }
+
+        [Test]
+        public void InstantiateVetWithNoArgsConstructorContainsCorrectValues()
+        {
+            Vet vet = new Vet();
+
+            Assert.AreEqual(vet.EmployeeId, "Vet_Test");
+            Assert.AreEqual(vet.Name, "Vet Test");
+            Assert.AreEqual(vet.Salary, -1.00);
+            Assert.AreEqual(vet.EmployeeType, "Vet");
+            Assert.AreEqual(vet.Location, "Dummy Ave, Co Limerick, Ireland");
+        }
+
+        [Test]
+        public void InstantiateVetWithAllArgsConstructorSuccessful()
+        {
+            Vet vet = new Vet("second_test", "Casey Duffy", 75000.78, "Vet", "Co Dundalk, Ireland");
+
+            Assert.AreEqual(vet.EmployeeId, "second_test");
+            Assert.AreEqual(vet.Name, "Casey Duffy");
+            Assert.AreEqual(vet.Salary, 75000.78);
+            Assert.AreEqual(vet.EmployeeType, "Vet");
+            Assert.AreEqual(vet.Location, "Co Dundalk, Ireland");
         }
     }
 }
