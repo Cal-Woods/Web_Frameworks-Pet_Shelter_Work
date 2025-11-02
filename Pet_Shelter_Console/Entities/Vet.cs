10dc8c10dea5173d89c561bca41f4e378ab6013e
Cal Woods <D00270955@student.dkit.ie>
2025-11-02 22:18:28 +0000

    Feat: Added class Vet to Entities folder with two constructors.
    Added new class Vet to Entities folder to represent vet employee's.

---
+++ Pet_Shelter_Console/Entities/Vet.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+
+namespace Pet_Shelter_Console.Entities
+{
+    public class Vet : Employee
+    {
+        public Vet():base() 
+        {
+            EmployeeId = "Vet_Test";
+            Name = "Vet Test";
+            Salary = -1.00;
+            EmployeeType = "Vet";
+            Location = "Dummy Ave, Co Limerick, Ireland";
+        }
+
+        public Vet(string employeeId, string name, double salary, string employeeType, string location):base(employeeId, name, salary, employeeType, location)
+        {
+
+        }
+    }
+}
