d45e07771e70c0779bc33578c5c057b80913cba1
Cal Woods <D00270955@student.dkit.ie>
2025-11-02 16:03:43 +0000

    Refactor: Changed all protected identifiers to public in Employee abstract class.
    Class located in the Entities folder of the Pet_Shelter_Console project.

--- Pet_Shelter_Console/Entities/Employee.cs
+++ Pet_Shelter_Console/Entities/Employee.cs
@@ -7,26 +7,26 @@
         private double salary;
         private string employeeType;
         private string location;
-        protected string EmployeeId {
+        public string EmployeeId {
             get { return employeeId; } 
             set { employeeId = value; }
         }
-        protected string Name 
+        public string Name 
         {
             get { return name; }
             set { name = value; }
         }
-        protected double Salary 
+        public double Salary 
         {
             get { return salary; }
             set { salary = value; }
         }
-        protected string EmployeeType 
+        public string EmployeeType 
         {
             get { return employeeType; }
             set { employeeType = value; }
         }
-        protected string Location 
+        public string Location 
         {
             get { return location; }
             set { location = value; }
@@ -36,7 +36,7 @@
         /// A no parameter constructor that instantiates test properties for an employee.
         /// </summary>
         /// <remarks>Not to be used in production!</remarks>
-        protected Employee() 
+        public Employee() 
         {
             EmployeeId = "-1";
             Name = "Dummy Manager object for testing, change all values before production usage!";
@@ -44,7 +44,7 @@
             EmployeeType = "Test";
             Location = "Dummy Lane, Co. Louth, Ireland";
         }
-        protected Employee(string employeeId, string name, double salary, string employeeType, string location)
+        public Employee(string employeeId, string name, double salary, string employeeType, string location)
         {
             EmployeeId = employeeId;
             Name = name;
