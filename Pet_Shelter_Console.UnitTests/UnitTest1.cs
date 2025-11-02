dc5d2e8707a3988cabce22024c485a7aa2db8c22
Cal Woods <D00270955@student.dkit.ie>
2025-11-02 14:38:04 +0000

    Project: Added new project for unit testing code.
    Added a project that uses NUnit3 libraries for unit testing to solution Pet_Shelter_Work.
    
    Will contain unit tests for written code in Pet_shelter_Console project.

---
+++ Pet_Shelter_Console.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Pet_Shelter_Console.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void PrintListOfEmployeesTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
